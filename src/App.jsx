cec5190036bb4fcd608757a2dc3c1e8e98af769e
Danish <ammarsaifi5376@gmail.com>
2025-02-27 16:53:42 +0530

    first push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { TimelineDemo } from "./Timeline";
+const App = () => {
+  return (
+    <>
+      <TimelineDemo />
+    </>
+  );
+};
+
+export default App;
